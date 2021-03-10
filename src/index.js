3a7cc0620c31baaed198d4358753a817915fff7c
angrythunderbird <headcrab.albert@gmail.com>
2021-03-11 01:42:37 +0200

    feat: something wrong, maybe node 10 install solve the problem... or not

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+
+  bracketsConfig.forEach(function (i) {
+    return i.reverse();
+  });
+
+  const bracesObj = Object.fromEntries(bracketsConfig);
+
+  const bracesClose = Object.keys(bracesObj);
+  const bracesOpen = Object.values(bracesObj);
+
+  return str.split('').reduce((stack, item) => {
+    bracesClose.includes(item) && stack[stack.length - 1] === bracesObj[item] ? stack.pop() : stack.push(item);
+    return stack;
+  }, []).length === 0;
+
+};
